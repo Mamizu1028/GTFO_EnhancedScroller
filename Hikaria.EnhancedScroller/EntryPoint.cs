eafc185a1e033deec16856b2f94147400ce09e96
Nina <859994525zjy@gmail.com>
2025-09-12 11:41:43 +0800

    添加项目文件。

---
+++ Hikaria.EnhancedScroller/EntryPoint.cs
@@ -0,0 +1,20 @@
+﻿using TheArchive.Core;
+using TheArchive.Core.Attributes;
+using TheArchive.Core.Localization;
+using TheArchive.Interfaces;
+using TheArchive.Loader;
+
+namespace Hikaria.ES;
+
+[ArchiveModule(PluginInfo.GUID, PluginInfo.NAME, PluginInfo.VERSION)]
+internal class EntryPoint : IArchiveModule
+{
+    public ILocalizationService LocalizationService { get; set; }
+    public IArchiveLogger Logger { get; set; }
+
+    public void Init()
+    {
+        LoaderWrapper.ClassInjector.RegisterTypeInIl2Cpp<EnhancedScroller>();
+        LoaderWrapper.ClassInjector.RegisterTypeInIl2Cpp<EnhancedScrollerCellView>();
+    }
+}
---
+++ Hikaria.EnhancedScroller/PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace Hikaria.ES;
+
+public class PluginInfo
+{
+    public const string GUID = "Hikaria.EnhancedScroller";
+
+    public const string NAME = "EnhancedScroller";
+
+    public const string VERSION = "1.0.0";
+}
